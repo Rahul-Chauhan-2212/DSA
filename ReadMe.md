1481d5b102dbb17042d6b0f35a32bcf8b8d820dc
Rahul Chauhan <rchauhan9102@gmail.com>
2025-09-12 13:58:35 +0530

    Two Sum

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# DSA
+
+1. [**Two Sum**](https://github.com/Rahul-Chauhan-2212/DSA/blob/master/src/main/java/arrays_strings/TwoSum.java)   - Hashing and Two Pointers
+2. 
---
+++ src/main/java/arrays_strings/TwoSum.java
@@ -0,0 +1,73 @@
+package arrays_strings;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+  // Given an array of integers nums and an integer target,
+  // return indices of the two numbers such that they add up to target
+  /*
+  Input: nums = [2,7,11,15], target = 9
+  Output: [0,1]
+  Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
+   */
+  public static void main(String[] args) {
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+    printOutput(bruteForce(nums, target));
+    printOutput(better(nums, target));
+    System.out.println(optimal(new int[]{3, 2, 4}, 6));
+  }
+
+  private static void printOutput(int[] arr) {
+    System.out.println(Arrays.stream(arr)
+        .boxed()
+        .toList());
+  }
+
+  //O(n^2)
+  private static int[] bruteForce(int[] arr, int k) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] == k) {
+          return new int[]{i, j};
+        }
+      }
+    }
+    return new int[]{-1, -1};
+  }
+
+  //O(n)
+  private static int[] better(int[] arr, int k) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < arr.length; i++) {
+      int complement = k - arr[i];
+      if (map.containsKey(complement)) {
+        return new int[]{map.get(complement), i};
+      }
+      map.put(arr[i], i);
+    }
+    return new int[]{-1, -1};
+  }
+
+  //O(n) + O(nlog(n))  -- 2 Pointer
+  private static boolean optimal(int[] arr, int k) {
+    Arrays.sort(arr);
+    int left = 0;
+    int right = arr.length - 1;
+    while (left < right) {
+      int sum = arr[left] + arr[right];
+      if (sum == k) {
+        return true;
+      } else if (sum < k) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+    return false;
+  }
+
+}
